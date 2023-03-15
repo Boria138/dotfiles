f6cd13d21f3caf740d240e99e6b5547d094778a9
Boria138 <boriabloger@protonmail.com>
2023-03-15 22:37:40 +0600

    Replaced notify-send with dunstify

--- i3/.config/i3/scripts/volume.sh
+++ i3/.config/i3/scripts/volume.sh
@@ -14,18 +14,14 @@ function is_mute {
 }
 
 function send_notification {
-    DIR=`dirname "$0"`
     volume=`get_volume`
-    # Make the bar with the special character ─ (it's not dash -)
-    # https://en.wikipedia.org/wiki/Box-drawing_character
-#bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
 if [ "$volume" = "0" ]; then
         icon_name="/usr/share/icons/Papirus/48x48/status/notification-audio-volume-muted.svg"
-$DIR/notify-send.sh "$volume""      " -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace=555
+		dunstify "$volume""      " -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"─" --replace=555
     else
 	if [  "$volume" -lt "10" ]; then
 	     icon_name="/usr/share/icons/Papirus/48x48/status/notification-audio-volume-low.svg"
-$DIR/notify-send.sh "$volume""     " -i "$icon_name" --replace=555 -t 2000
+		 dunstify "$volume""     " -i "$icon_name" --replace=555 -t 2000
     else
         if [ "$volume" -lt "30" ]; then
             icon_name="/usr/share/icons/Papirus/48x48/status/notification-audio-volume-low.svg"
@@ -40,31 +36,26 @@ $DIR/notify-send.sh "$volume""     " -i "$icon_name" --replace=555 -t 2000
 fi
 bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
 # Send the notification
-$DIR/notify-send.sh "$volume""     ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace=555
+dunstify "$volume""     ""$bar" -i "$icon_name" -t 2000 -h int:value:"$volume" -h string:synchronous:"$bar" --replace=555
 
 }
 
 case $1 in
-    up)
-	# Set the volume on (if it was muted)
-	amixer -D pulse set Master on > /dev/null
-	# Up the volume (+ 5%)
-	amixer -D pulse sset Master 5%+ > /dev/null
-	send_notification
-	;;
-    down)
-	amixer -D pulse set Master on > /dev/null
-	amixer -D pulse sset Master 5%- > /dev/null
-	send_notification
-	;;
-    mute)
-    	# Toggle mute
-	amixer -D pulse set Master 1+ toggle > /dev/null
-	if is_mute ; then
-    DIR=`dirname "$0"`
-    $DIR/notify-send.sh -i "/usr/share/icons/Papirus/48x48/status/notification-audio-volume-muted.svg" --replace=555 -u normal "Mute" -t 2000
-	else
-	    send_notification
-	fi
-	;;
+  up)
+    # set the volume on (if it was muted)
+    amixer -D pulse set Master on > /dev/null
+    # up the volume (+ 5%)
+    amixer -D pulse sset Master 5%+ > /dev/null
+    send_notification
+    ;;
+  down)
+    amixer -D pulse set Master on > /dev/null
+    amixer -D pulse sset Master 5%- > /dev/null
+    send_notification
+    ;;
+  mute)
+    # toggle mute
+    amixer -D pulse set Master 1+ toggle > /dev/null
+    send_notification
+    ;;
 esac
